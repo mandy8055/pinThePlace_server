6334687f10d7adc2e5a9e7d14817dae6ade979f7
Mandy8055 <msankrit8055@gmail.com>
2021-06-29 21:16:06 +0530

    finalized server side scripting and functionalities

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("MongoDB  is now connected");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Backend server is up and running!!!");
+});
